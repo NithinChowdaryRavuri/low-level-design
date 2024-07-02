675997ba8f700a4feb40614748e5a796b8ab0a2c
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-07-02 16:37:34 -0500

    Car Rental System

---
+++ carrentalsystem/Car.java
@@ -0,0 +1,47 @@
+package carrentalsystem;
+
+public class Car {
+    private final String make;
+    private final String model;
+    private final int year;
+    private final String licensePlate;
+    private final double rentalPricePerDay;
+    private boolean available;
+
+    public Car(String make, String model, int year, String licensePlate, double rentalPricePerDay) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.licensePlate = licensePlate;
+        this.rentalPricePerDay = rentalPricePerDay;
+        this.available = true;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public double getRentalPricePerDay() {
+        return rentalPricePerDay;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
