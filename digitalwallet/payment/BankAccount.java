da832a7357d78c996b7120a51ed8c1ee41bcba31
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-07-04 15:04:55 -0500

    Digital Wallet

---
+++ digitalwallet/payment/BankAccount.java
@@ -0,0 +1,23 @@
+package digitalwallet.payment;
+
+import digitalwallet.Currency;
+import digitalwallet.User;
+
+import java.math.BigDecimal;
+
+public class BankAccount extends PaymentMethod{
+    private final String accountNumber;
+    private final String routingNumber;
+
+    public BankAccount(String id, User user, String accountNumber, String routingNumber) {
+        super(id, user);
+        this.accountNumber = accountNumber;
+        this.routingNumber = routingNumber;
+    }
+
+    @Override
+    public boolean processPayment(BigDecimal amount, Currency currency) {
+        // Process bank account payment
+        return true;
+    }
+}
