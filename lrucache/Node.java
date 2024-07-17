b8f19fba4254aa8e856d5eaf7aac3c368778eccb
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-07-17 13:59:38 -0500

    LRU Cache

---
+++ lrucache/Node.java
@@ -0,0 +1,13 @@
+package lrucache;
+
+public class Node<K, V> {
+    K key;
+    V value;
+    Node<K, V> prev;
+    Node<K, V> next;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
