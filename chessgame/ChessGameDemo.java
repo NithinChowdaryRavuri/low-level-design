ad7d57d1b615f2b23ac4bd773c0e5184ecfa4c59
Nithin_Chowdary_Ravuri <ravurinithinchowdary@gmail.com>
2024-07-19 20:48:34 -0500

    Chess Game

---
+++ chessgame/ChessGameDemo.java
@@ -0,0 +1,8 @@
+package chessgame;
+
+public class ChessGameDemo {
+    public static void main(String[] args) {
+        ChessGame game = new ChessGame();
+        game.start();
+    }
+}
---
+++ chessgame/pieces/Piece.java
@@ -0,0 +1,39 @@
+package chessgame.pieces;
+
+import chessgame.Board;
+import chessgame.Color;
+import chessgame.Move;
+
+public abstract class Piece {
+    protected final Color color;
+    protected int row;
+    protected int col;
+
+    public Piece(Color color, int row, int col) {
+        this.color = color;
+        this.row = row;
+        this.col = col;
+    }
+
+    public abstract boolean canMove(Board board, int destRow, int destCol);
+
+    public Color getColor() {
+        return color;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+}
